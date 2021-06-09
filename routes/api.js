712bc1e3e3dcc862679ce9fd52f0a6b0b14fbca5
hafizhmh <hafizh.mulya@yahoo.com>
2021-06-09 07:42:55 +0000

    Enable /api/harga and /api/panen
    /api/harga is functional, connected to bucket gs://panenin.
    /api/panen is still using dummy to simulate output.

--- routes/api.js
+++ routes/api.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const csv = require('csv-parser');
 const express = require('express');
 const router = express.Router();
 
@@ -6,20 +7,54 @@ const { spawn } = require('child_process');
 
 // connection test
 router.get('/test', function (req, res, next) {
+  console.log('get /test');
   res.send({ status: "OK" });
 });
 
 
 ////////////////////
-// sample
-router.get('/sample', function (req, res, next) {
-  console.log('get /sample', req.query)
-  res.send({
-    sample1: "OK1",
-    sample2: "OK2",
-  });
+
+// panen
+router.get('/panen', function (req, res, next) {
+  console.log('get /panen', req.query)
+  res.send([1.1, 1.2, 1.3, 1.4, 1.9]);
 });
 
+
+
+router.get('/harga', function (req, res, next) {
+  console.log('get /harga', req.query);
+  var result  = [];
+  var path = req.query.kota + "_" + req.query.crop + ".csv";
+  var bucket_path = "gs://panenin/" + path;
+  console.log(bucket_path);
+  var fetch =  spawn('gsutil', ['cp', bucket_path, '.'])
+  fetch.on('exit', () => {
+    console.log("fecthing bucket done");
+    if (fs.existsSync(path)) {
+      console.log("file exist");
+      fs.createReadStream(path)
+      .pipe(csv())
+      .on('data', (row) => {
+        if (typeof req.query.tahun == "undefined") {
+          result.push(row);
+          console.log(row);
+        } else {
+          if (row['tahun'] == req.query.tahun ) {
+            result.push(row);
+            console.log(row);
+          }
+        };
+      })
+      .on('end', () => {
+        res.send(result);
+      });
+    } else {
+      console.log("file does not exists");
+      res.status(422).send({error: "Wilayah/crop tidak ditemukan"});
+    }
+  });
+});
 ////////////////////
 
 // ml agent
